747c6c16f516b0691a2257f17694c86e0c31d976
patrikarvidsson <patrik@arvidson.nu>
2019-04-20 13:09:04 +0200

    Adding netlify plugin

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -32,6 +32,21 @@ module.exports = {
         icon: `src/images/logo.flat.svg`
       }
     },
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {
+          '/*.css': ['Cache-Control: public, max-age=31536000, immutable'],
+          '/*.js': ['Cache-Control: public, max-age=31536000, immutable'],
+        }, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
     'gatsby-plugin-offline'
   ]
 }

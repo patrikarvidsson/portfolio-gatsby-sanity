ef133e4b24d17b7e0069d762f7ce34f6e128d9ec
patrikarvidsson <patrik@arvidson.nu>
2019-04-22 19:23:05 +0200

    Adding open-source link

--- web/src/components/layout.js
+++ web/src/components/layout.js
@@ -32,7 +32,9 @@ const Layout = ({ children, companyInfo, onHideNav, onShowNav, showNav, siteTitl
         <div className={styles.siteInfo}>
           © {new Date().getFullYear()}, Built with <a href='https://www.sanity.io'>Sanity</a> &amp;
           {` `}
-          <a href='https://www.gatsbyjs.org'>Gatsby</a>
+          <a href='https://www.gatsbyjs.org'>Gatsby</a> ·
+          {` `}
+          <a href='https://github.com/patrikarvidsson/portfolio-gatsby-sanity'>Open-source</a>
         </div>
       </div>
     </footer>

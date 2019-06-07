b619c2a230427decdbf0e6918c5a87987617eb0a
patrikarvidsson <patrik@arvidson.nu>
2019-06-07 17:16:32 +0200

    Removing quotes from Sitemap

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -42,7 +42,7 @@ module.exports = {
         // Exclude specific pages or groups of pages using glob parameters
         // See: https://github.com/isaacs/minimatch
         // The example below will exclude the single `path/to/page` and all routes beginning with `category`
-        exclude: ['/category/*', `/tag/*`],
+        exclude: ['/category/*', `/tag/*`, `/quote/*`],
         query: `
         {
           site {

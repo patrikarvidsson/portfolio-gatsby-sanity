e9c0fbbb8b402a2931e072590bd792654875f8d5
patrikarvidsson <patrik@arvidson.nu>
2019-07-04 17:40:12 +0200

    Adding now.js and updating packges

---
+++ api/python/index.py
@@ -0,0 +1,11 @@
+import datetime
+from http.server import BaseHTTPRequestHandler
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/plain')
+        self.end_headers()
+        self.wfile.write(str(datetime.datetime.now()).encode())
+        return
